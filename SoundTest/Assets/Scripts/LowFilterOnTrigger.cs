54d2e31434798ea277212cc1edd9adfecb58016c
Fernando <fernando.gonzalez@enti.cat>
2021-05-31 20:28:25 +0200

    Project integration
    Added all project files to repository

---
+++ SoundTest/Assets/Scripts/LowFilterOnTrigger.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LowFilterOnTrigger : MonoBehaviour
+{
+    [SerializeField] private GameObject playerObject;
+    [SerializeField] private GameObject triggerObject;
+    private AudioLowPassFilter lpf;
+    private int defaultValue = 22000;
+    private int newValue = 3000;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lpf = GetComponent<AudioLowPassFilter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(playerObject.transform.position, triggerObject.transform.position) < 3)
+        {
+            StartCoroutine(decreaseCutoff(lpf, newValue));
+        }
+        else
+        {
+            if(lpf.cutoffFrequency < defaultValue)
+            {
+                StartCoroutine(increaseCutoff(lpf, defaultValue));
+            }
+        }
+    }
+
+    static IEnumerator decreaseCutoff(AudioLowPassFilter _lpf, int targetval)
+    {
+        while (_lpf.cutoffFrequency > targetval)
+        {
+            _lpf.cutoffFrequency -= 100;
+            yield return new WaitForSeconds(.1f);
+        }
+    }
+
+    static IEnumerator increaseCutoff(AudioLowPassFilter _lpf, int targetval)
+    {
+        while (_lpf.cutoffFrequency < targetval)
+        {
+            _lpf.cutoffFrequency += 100;
+            yield return new WaitForSeconds(.1f);
+        }
+    }
+}
---
+++ SoundTest/Assets/Scripts/SoundInput.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundInput : MonoBehaviour
+{
+    private AudioSource iAS;
+    public AudioClip[] burpClips;
+    public AudioClip jumplClip;
+    private Rigidbody rb;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        iAS = GetComponentInChildren<AudioSource>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            if (rb.velocity.y > -0.3 && rb.velocity.y < 0.3)
+            {
+                iAS.PlayOneShot(jumplClip);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            iAS.PlayOneShot(burpClips[UnityEngine.Random.Range(0, burpClips.Length)]);
+        }
+    }
+}
